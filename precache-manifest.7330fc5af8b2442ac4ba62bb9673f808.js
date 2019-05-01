5298f9c734d1a2de9f11c9084d1a62f3d471129b
connormayfield <connormayfield@gmail.com>
2019-05-01 03:48:13 -0500

    Updates

---
+++ precache-manifest.7330fc5af8b2442ac4ba62bb9673f808.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5526075650dd64e279dc1136f52268d6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9a60592ab5789e8f51c1",
+    "url": "/clicky-game/static/css/main.ea387116.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "9a60592ab5789e8f51c1",
+    "url": "/clicky-game/static/js/main.e39548d3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
